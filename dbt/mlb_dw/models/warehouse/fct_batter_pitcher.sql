787b2f5966996484db3ab04ccf9e7277aac6fd0b
Ryan Aguilar <ryanaguilar323@gmail.com>
2023-08-05 21:29:33 -0400

    Fix next game hits

--- dbt/mlb_dw/models/warehouse/fct_batter_pitcher.sql
+++ dbt/mlb_dw/models/warehouse/fct_batter_pitcher.sql
@@ -15,6 +15,9 @@ from batter_pitcher as bp
 ),
 box_scores as (
     select * from {{ ref('stg_box_scores') }}
+),
+next_game_hit as (
+    select * from {{ ref('fct_next_game_hit')}}
 )
 select
     ng.k_batter,
@@ -29,14 +32,12 @@ select
     p.swinging_strikes as Strikes_Swinging,
     p.hits_per_inning_pitched,
     p.pitchers_per_appearance as pitches_per_appearance_avg,
-    bs.sz_babip as babip,
-    bs.sz_avg as ba,
-    (bs.sz_at_bats - bs.sz_strike_outs - bs.sz_home_runs + bs.sz_sac_flies) as bip,
+    bs.sz_babip as BABIP,
+    bs.sz_avg as BA,
+    (bs.sz_at_bats - bs.sz_strike_outs - bs.sz_home_runs + bs.sz_sac_flies) as BIP,
     p.last_three_innings_pitched as hip_last_three,
-    case
-        when lead(bs.current_game_hits) over (partition by bs.k_batter order by bs.game_date) > 0 then 1
-        else 0
-    end as nextgame_HIT
+    ngh.next_game_hits,
+    ngh.next_game_hit_indicator as next_game_HIT
 from next_game as ng
 left join dim_pitcher as p
 on ng.next_game_date = p.game_date
@@ -44,3 +45,6 @@ and ng.next_game_starting_pitcher = p.k_pitcher
 left join box_scores as bs
 on ng.next_game_date = bs.game_date
 and ng.k_batter = bs.k_batter
+left join next_game_hit ngh
+on ng.k_batter = ngh.k_batter
+and ng.game_date = ngh.game_date
